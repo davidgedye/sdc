ebf01a70f1686d175292eafd001bb1f99d5362f7
David Gedye <david@gedye.com>
2026-02-14 12:26:06 -0800

    Deploy 2026-02-14

--- montages/images.js
+++ montages/images.js
@@ -5,12 +5,12 @@ var images = [
     { "dzi": "birthday.dzi", "w": 5875, "h": 3000 },
     { "dzi": "chrisLA.dzi", "w": 5692, "h": 4288 },
     { "dzi": "foggydisco.dzi", "w": 8063, "h": 3024 },
+    { "dzi": "foggytrees.dzi", "w": 3040, "h": 4029 },
     { "dzi": "fuji1.dzi", "w": 4024, "h": 4470 },
     { "dzi": "fuji_window.dzi", "w": 6048, "h": 4031 },
     { "dzi": "leaf.dzi", "w": 4044, "h": 5345 },
     { "dzi": "niagara.dzi", "w": 4132, "h": 2583 },
     { "dzi": "niagara2.dzi", "w": 4287, "h": 2847 },
     { "dzi": "tableware.dzi", "w": 3027, "h": 2798 },
-    { "dzi": "train1.dzi", "w": 6921, "h": 2593 },
     { "dzi": "water.dzi", "w": 7759, "h": 5560 }
 ];
